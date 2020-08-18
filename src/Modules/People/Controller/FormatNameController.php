78ed99f3174c4a766ed791667b474a315018403f
Craig Rayner <craig@craigrayner.com>
2020-08-19 09:35:23 +1000

    Format Name Setting Update

--- src/Modules/People/Controller/FormatNameController.php
+++ src/Modules/People/Controller/FormatNameController.php
@@ -17,11 +17,9 @@
 namespace App\Modules\People\Controller;
 
 use App\Container\Container;
-use App\Container\ContainerManager;
 use App\Container\Panel;
 use App\Container\Section;
 use App\Controller\AbstractPageController;
-use App\Modules\People\Form\FormatNameSettingType;
 use App\Modules\People\Manager\PersonNameManager;
 use App\Modules\System\Manager\SettingFactory;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -38,33 +36,34 @@ class FormatNameController extends AbstractPageController
 {
     /**
      * settings
-     * @param ContainerManager $manager
+     *
+     * 19/08/2020 09:30
      * @param string $tabName
-     * @return JsonResponse
      * @Route("/format/name/settings/{tabName}",name="format_name_settings")
      * @IsGranted("ROLE_ROUTE")
-     * 25/07/2020 13:02
+     * @return JsonResponse
      */
-    public function settings(ContainerManager $manager, string $tabName = 'General')
+    public function settings(string $tabName = 'General')
     {
         $container = new Container($tabName);
         foreach(PersonNameManager::getPersonTypeList() as $type) {
             $form = $this->getForm($type);
             $panel = new Panel($type, 'People', new Section('form', $type));
-            $container->addForm($type, $form->createView())->addPanel($panel);
+            $container->addForm($type, $form->createView())
+                ->addPanel($panel);
         }
-        $manager->addContainer($container);
 
         return $this->getPageManager()
             ->createBreadcrumbs('Format Name Settings')
-            ->render(['containers' => $manager->getBuiltContainers()]);
+            ->render(['containers' => $this->getContainerManager()->addContainer($container)->getBuiltContainers()]);
     }
 
     /**
      * getForm
+     *
+     * 19/08/2020 09:32
      * @param string $tabName
      * @return FormInterface
-     * 25/07/2020 13:10
      */
     private function getForm(string $tabName)
     {
@@ -80,25 +79,28 @@ class FormatNameController extends AbstractPageController
 
     /**
      * saveFormatNameSettings
-     * @param ContainerManager $manager
+     *
+     * 19/08/2020 09:31
      * @param string $tabName
-     * @return JsonResponse
      * @Route("/format/name/{tabName}/save/",name="format_name_save")
      * @IsGranted("ROLE_ROUTE")
-     * 25/07/2020 13:12
+     * @return JsonResponse
      */
-    public function saveFormatNameSettings(ContainerManager $manager, string $tabName)
+    public function saveFormatNameSettings(string $tabName)
     {
         $form = $this->getForm($tabName);
 
-        $data = SettingFactory::getSettingManager()->handleSettingsForm($form, $this->getRequest(), []);
-        if ($data['status'] === 'success') {
+        SettingFactory::getSettingManager()->handleSettingsForm($form, $this->getRequest());
+        if ($this->isStatusSuccess()) {
             $form = $this->getForm($tabName);
         }
 
-        $manager->singlePanel($form->createView());
-        $data['form'] = $manager->getFormFromContainer();
-
-        return new JsonResponse($data);
+        return $this->generateJsonResponse(
+            [
+                'form' => $this->getContainerManager()
+                    ->singlePanel($form->createView())
+                    ->getFormFromContainer()
+            ]
+        );
     }
 }
